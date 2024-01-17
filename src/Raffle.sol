23b2a96b2ba7e4559759784b9a3c66d787a98a78
TechTunist <mrmpjackson@gmail.com>
2024-01-17 13:33:34 +0000

    chainlink VRF automation setup

--- src/Raffle.sol
+++ src/Raffle.sol
@@ -18,9 +18,15 @@ import {VRFConsumerBaseV2} from "@chainlink/contracts/src/v0.8/vrf/VRFConsumerBa
 
 
 contract Raffle is VRFConsumerBaseV2 {
-    error Raffle__NotEnoughEthToEnter(string message);
+    error Raffle__NotEnoughEthToEnter();
     error Raffle__TransferFailed();
     error Raffle__RaffleNotOpen();
+
+    error Raffle__RaffleUpkeepNotNeeded(
+        uint256 currentBalance,
+        uint256 numPlayers,
+        uint256 raffleState
+        );
 
     /** Type Declarations */
     enum RaffleState {
@@ -47,6 +53,7 @@ contract Raffle is VRFConsumerBaseV2 {
 
     /** Events */
     event EnteredRaffle (address indexed player);
+    event PickedWinner (address indexed winner);
 
     constructor(
         uint256 entranceFee,
@@ -68,7 +75,7 @@ contract Raffle is VRFConsumerBaseV2 {
     
     function enterRaffle() external payable{
         if(msg.value < i_entranceFee) {
-            revert Raffle__NotEnoughEthToEnter("Not enough ETH to enter");
+            revert Raffle__NotEnoughEthToEnter();
         }
         if (s_raffleState != RaffleState.OPEN) {
             revert Raffle__RaffleNotOpen();
@@ -77,14 +84,39 @@ contract Raffle is VRFConsumerBaseV2 {
         emit EnteredRaffle(msg.sender);
     }
 
-    function pickWinner() external {
-        // check if enough time has passed
-        if ((block.timestamp - s_lastTimeStamp) > i_interval) {
-            revert();
-        }
+    // when is the winner supposed to be picked?
+    /**
+     * @dev this is the function that the Chainlink node will call to see if it is time to perform an upkeep.
+     * The following should be true for this to return true:
+     * 1. the time interval has passed between raffle runs
+     * 2. the raffle is in an OPEN state
+     * 3. the contract has ETH (aka, players in the s_players array)
+     * 4. (Implicit) the subscription is funded with LINK
+     */
+    
+    function checkUpkeep(bytes memory /* checkData */) public view returns(bool upkeepNeeded, bytes memory /* performData */ ) {
+        bool timeHasPassed = ((block.timestamp - s_lastTimeStamp) >= i_interval);
+        bool isOpen = (s_raffleState == RaffleState.OPEN);
+        bool hasPlayers = s_players.length > 0;
+
+        upkeepNeeded = (timeHasPassed && isOpen && hasPlayers);
+
+        return(upkeepNeeded, "");
+    }
+
+    function performUpkeep(bytes calldata /* performData */) external {
+        (bool upkeepNeeded, ) = checkUpkeep("");
+        if(!upkeepNeeded) {
+            revert Raffle__RaffleUpkeepNotNeeded(
+                address(this).balance,
+                s_players.length,
+                uint256(s_raffleState)
+                );
+            }
+        s_raffleState = RaffleState.CALCULATING;
 
         // if enough time has passed, pick a winner using a random number
-        uint256 requestId = i_vrfCoordinator.requestRandomWords(
+        i_vrfCoordinator.requestRandomWords(
             i_gasLane, // gas lane
             i_subscriptionId, // the deployed subscription contract
             REQUEST_CONFIRMATIONS,
@@ -93,12 +125,21 @@ contract Raffle is VRFConsumerBaseV2 {
         );
     }
 
-    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords) internal override {
+    function fulfillRandomWords(uint256 /* requestId */, uint256[] memory randomWords) internal override {
         uint256 indexOfWinner = randomWords[0] % s_players.length;
         address payable winner = s_players[indexOfWinner];
 
         s_recentWinner = winner;
-        (bool success,) = winner.call{value: address(this).balance}("");
+        s_raffleState = RaffleState.OPEN;
+
+        // clear the players array by reinitializing it
+        s_players = new address payable[](0);
+
+        // update the last time stamp
+        s_lastTimeStamp = block.timestamp;
+        emit PickedWinner(winner);
+
+        (bool success,) = (winner.call{value: address(this).balance}(""));
         if (!success) {
             revert Raffle__TransferFailed();
         }
@@ -110,3 +151,13 @@ contract Raffle is VRFConsumerBaseV2 {
         return i_entranceFee;
     }
 }
+
+// checks, effect, interactions
+
+/** CHECKS: (require, revert, errors etc) come first because it is more gas efficient
+ * to revert before a ton of calculations are done
+ * 
+ * EFFECTS: (state changes of our own contract)
+ * 
+ * INTERACTIONS: (external calls on other contracts)
+ */
